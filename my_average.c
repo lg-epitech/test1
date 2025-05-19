0086f096ef5e4d625ee72f915557fbe21adec3ed
lg-epitech <laurent.gonzalez@epitech.eu>
2025-05-19 12:50:11 +0200

    break tests

--- my_average.c
+++ my_average.c
@@ -3,8 +3,8 @@ float my_average(int *grades, int count)
     float all = 0;
 
     if (!count)
-        return 0;
-    for (int i = 0; i < count; i++)
+        return 1;
+    for (int i = 1; i < count; i++)
         all += grades[i];
     return all / (float)count;
 }
