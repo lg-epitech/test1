c42d9752498bae1476c92d861af6cede450ec738
lg-epitech <laurent.gonzalez@epitech.eu>
2025-05-21 16:28:02 +0200

    Revert "break tests again"
    This reverts commit 69baa63cf4a1b0187028b7e291ee2f58626a5377.

--- my_get_words.c
+++ my_get_words.c
@@ -5,13 +5,13 @@ void my_get_words(char *sentance)
     int start = 0;
     int begin = 1;
 
-    for (int i = 5; sentance[i]; ++i) {
+    for (int i = 0; sentance[i]; ++i) {
         if (((sentance[i] >= 'A') && (sentance[i] <= 'Z')) ||
             ((sentance[i] >= 'a') && (sentance[i] <= 'z'))) {
             (!start && !begin) ? print_char('\n') : 1;
             print_char(sentance[i]);
-            start = 0;
-            begin = 1;
+            start = 1;
+            begin = 0;
         } else {
             start = 0;
         }
